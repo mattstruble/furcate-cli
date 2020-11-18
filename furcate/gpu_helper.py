b629f1d22c1f5d22b1cda1d6b23c36e0ffc04f98
Matt Struble <mattstruble@outlook.com>
2020-11-18 17:19:05 -0500

    Adding set_gpu functionality to limit gpu scope during parallelization.

--- furcate/gpu_helper.py
+++ furcate/gpu_helper.py
@@ -6,3 +6,12 @@ def get_gpus(framework):
         raise TypeError("The supplied framework '{}' is not supported.".format(framework))
 
     return gpus
+
+def set_gpus(index, framework):
+    gpus = get_gpus(framework)
+
+    if framework == 'tf':
+        import tensorflow as tf
+        tf.config.set_visible_devices(gpus[int(index)], 'GPU')
+    else:
+        raise TypeError("The supplied framework '{}' is not supported.".format(framework))
