37b8fb526ddbd4a4299f7d7944fcd4bba3a4940a
Matt Struble <mattstruble@outlook.com>
2020-11-18 20:52:53 -0500

    Updated setup to pull version from file.

--- setup.py
+++ setup.py
@@ -6,16 +6,19 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read
 
+with open('VERSION') as f:
+    version = f.read
+
 setup(
     name='furcate',
-    version='0.0.1',
-    description='A lightweight wrapper for automatically forking TensorFlow training sessions based on user configurations.',
+    version=version,
+    description='A lightweight wrapper for automatically forking deep learning training sessions based on user configurations.',
     long_description=readme,
     author='Matt Struble',
     author_email='mattstruble@outlook.com',
     url='https://github.com/mattstruble/furcate',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs')),
+    packages=find_packages(exclude=('tests', 'docs', 'example')),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
