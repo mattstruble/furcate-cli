d743ea2acb7c1dc66101171431e5b5b17bb77f0b
Matt Struble <mattstruble@outlook.com>
2020-11-30 12:17:56 -0500

    Split logging information into logging.config and allow users to define custom configurations.

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     url='https://github.com/mattstruble/furcate',
     license=license,
     packages=find_packages(exclude=('tests', 'docs', 'examples')),
+    include_package_data=True,
     install_requires=[
         'pandas',
         'matplotlib'
