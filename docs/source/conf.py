3c129b6d06682a43e4b507449613a90031e1c7b2
Matt Struble <mattstruble@outlook.com>
2021-03-24 16:52:44 -0400

    Update conf.py
    Updating pathing to module so that readthedocs can pick it up.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../furcate"))
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(1, os.path.abspath("../furcate"))
 
 
 # -- Project information -----------------------------------------------------
