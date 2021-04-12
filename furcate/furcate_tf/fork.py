e5246bf40789651a9783f6014a5a28db70cc832b
mattstruble <mattstruble@outlook.com>
2021-04-11 22:15:59 -0400

    Updated furcate-tf docstrings to reflect TF specific objects and methods.

--- furcate/furcate_tf/fork.py
+++ furcate/furcate_tf/fork.py
@@ -39,23 +39,72 @@ class Fork(furcate.fork.Fork):
         tf.random.set_seed(self.seed)
 
     def get_available_gpu_indices(self):
+        """
+        Gets a list of available GPUs indices to train on using `tf.config.list_physical_devices`
+
+        :return: List of available GPUs
+        :rtype: list
+        """
         gpus = tf.config.list_physical_devices("GPU")
         indices = [int(re.findall(r"\d+", gpu.name)[0]) for gpu in gpus]
         return indices
 
     def set_visible_gpus(self):
+        """
+        Uses `self.gpu_id` to set the trainign session's visible GPUs using `tf.config.set_visible_devices`
+
+        :return: None
+        """
         if self.gpu_id > -1:
             gpus = tf.config.list_physical_devices("GPU")
             visible_devices = [gpu for gpu in gpus if str(self.gpu_id) in gpu.name]
             tf.config.set_visible_devices(visible_devices, "GPU")
 
     def model_compile(self, model, optimizer, loss, metrics):
+        """
+        Compiles the model for training.
+
+        :param model: Model to be compiled
+        :type model: tf.keras.Model
+        :param optimizer: Training optimizer
+        :type optimizer: function
+        :param loss: Loss function to train against
+        :type loss: function
+        :param metrics: Metrics to record
+        :type metrics: list
+        :return: None
+        """
         model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 
     def model_summary(self, model):
+        """
+        Used for displaying the model architecture summary to the user. Called when verbose > 1.
+
+        :param model: Model to be displayed.
+        :type model: tf.keras.Model
+        :return: None
+        """
         model.summary()
 
     def model_fit(self, model, train_set, epochs, valid_set, callbacks, verbose):
+        """
+        Trains the compiled model and returns the history of training.
+
+        :param model: Model to train
+        :type model: tf.keras.Model
+        :param train_set: Training dataset to fit against
+        :type train_set: generator or list
+        :param epochs: Number of epochs to train
+        :type epochs: int
+        :param valid_set: Validation dataset
+        :type valid_set: generator or list
+        :param callbacks: A list of training callbacks
+        :type callbacks: list
+        :param verbose: Verbosity of training
+        :type verbose: int
+        :return: History object representing the model training
+        :rtype: dict
+        """
         history = model.fit(
             train_set,
             epochs=epochs,
@@ -66,13 +115,41 @@ class Fork(furcate.fork.Fork):
         return history
 
     def model_evaluate(self, model, test_set):
+        """
+        Evaluates the model against the provided test set.
+
+        :param model: Model to evaluate
+        :type model: tf.keras.Model
+        :param test_set: Dataset to test the model against
+        :type test_set: generator or list
+        :return: Evaluation results
+        """
         results = model.evaluate(test_set)
         return results
 
     def model_save(self, model):
+        """
+        Save the model to disk.
+
+        :param model: Trained model to save
+        :type model: tf.keras.Model
+        :return: None
+        """
         model.save(os.path.join(self.log_dir, "model.h5"))
 
     def gen_tfrecord_dataset(self, filepaths, processor, shuffle=False):
+        """
+        Generates a `TFRecordDataset` mapped via the passed in processor.
+
+        :param filepaths: list of filepaths
+        :type filepaths: list
+        :param processor: Function to map the dataset against
+        :type processor: function
+        :param shuffle: whether to shuffle the dataset
+        :type shuffle: bool
+        :return: A dataset that has been mapped, cached, batched, and prefetched.
+        :rtype: tf.data.TFRecordDataset
+        """
         dataset = tf.data.TFRecordDataset(
             filepaths, num_parallel_reads=self.num_parallel_reads
         )
